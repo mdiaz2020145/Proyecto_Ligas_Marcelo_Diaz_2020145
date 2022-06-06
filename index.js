138b44ae9ecfb2317abb0279af8fe4b6c88fe868
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-06-06 09:08:38 -0600

    Heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/IN6BM_proyectoLigas', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
 
-    app.listen(3000, function () {
+    app.listen(process.env.PORT,7000, function () {
         console.log("Esta corriendo en el puerto 3000!")
     })
 
